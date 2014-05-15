19dbf02c7ac36f44770534b54d5399315d3292f0
Sunny <cocos2der@gmail.com>
2014-05-15 09:40:48 +0800

    Xcode template for Cocos2dx Class (Scene or Layer)

---
+++ cocos2dx_class_xctemplate/2dx Layer Class.xctemplate/___FILEBASENAME___.cpp
@@ -0,0 +1,38 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___()
+{
+    
+}
+
+___FILEBASENAMEASIDENTIFIER___::~___FILEBASENAMEASIDENTIFIER___()
+{
+    
+}
+
+bool ___FILEBASENAMEASIDENTIFIER___::init()
+{
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    initData();
+    return true;
+}
+
+#pragma mark - initData
+void ___FILEBASENAMEASIDENTIFIER___::initData()
+{
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    
+    // add your codes here...
+    
+}
---
+++ cocos2dx_class_xctemplate/README.md
@@ -0,0 +1,7 @@
+Xcode template for Cocos2dx Class (Scene or Layer)
+====
+
+2dx Layer Class.xctemplate  
+2dx Scene Class.xctemplate
+
+put files in /Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File\ Templates/C\ and\ C++ 
